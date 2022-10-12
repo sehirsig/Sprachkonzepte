c3f9f3ee69f09162823b271f3513596fa99f7096
sehirsig <sebastianhirsig@bluewin.ch>
2022-10-12 11:18:46 +0200

    Finished Aufgabe 1 a

--- src/Aufgabe1/Formatspez.java
+++ src/Aufgabe1/Formatspez.java
@@ -5,16 +5,11 @@ import java.util.regex.Pattern;
 
 //    %[argument_index$][flags][width][.precision]conversion
 //    https://developer.android.com/reference/java/util/Formatter
+//    https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
 
 //Format fängt mit % an und hört mit d auf
 
 public class Formatspez {
-    static String ARGUMENT_INDEX = ".*?";
-    static String FLAGS = "[%]*";
-    static String WIDTH = "";
-    static String PRECISION = "";
-    static String CONVERSION = "[dn]";
-    static String PATTERN = "(%" + ARGUMENT_INDEX + FLAGS + WIDTH + PRECISION + CONVERSION + ")";
 
     public static void main(String[] args) {
         if (args.length != 0) {
@@ -22,30 +17,43 @@ public class Formatspez {
             System.exit(1);
         }
 
-        String formatString = "xxx %d yyy%n";//args[0];
-        int subStringStart = 0;
+
+        /*
+        xxx %d yyy%n
+        TEXT("xxx ")FORMAT("%d")TEXT(" yyy")FORMAT("%n")
+        xxx%1$d yyy
+        TEXT("xxx")FORMAT("%1$d")TEXT(" yyy")
+        %1$-02.3dyyy
+        FORMAT("%1$-02.3d")TEXT("yyy")
+         */
+        String formatString = "%1$-02.3dyyy";//args[0];
+
+        //% + ARGUMENT_INDEX + FLAGS + WIDTH + PRECISION + CONVERSION
+        // %[argument_index$][flags][width][.precision]conversion
+        // '%' + argument_index + flags + width + precision + conversion
+
+        String text = "([a-zA-Z ]*)";
+
+        String argument_index = "([1-9]\\d*\\$)*";
+        String flags = "[-#\\+ 0,\\(]*";
+        String width = "([1-9]\\d*)?";
+        String precision = "(\\.\\d*)?";
+        String conversion = "[bBhHsScCdoxXeEfgGaAtT%n]";
+        String PATTERN = "(%" + argument_index + flags + width + precision + conversion + ")|" + text;
 
         Pattern pattern = Pattern.compile(PATTERN);
         Matcher matcher = pattern.matcher(formatString);
 
         while (matcher.find()) {
-            printText(formatString.substring(subStringStart, matcher.start()));
-            printFormat(matcher.group());
+            //System.out.print(matcher.group());
 
-            subStringStart = matcher.end();
+            if (matcher.group().startsWith("%")) {
+                System.out.printf("FORMAT(\"%s\")", matcher.group());
+            } else {
+                if (!matcher.group().equals("")) {
+                System.out.printf("TEXT(\"%s\")", matcher.group());
+                }
+            }
         }
-        printText(formatString.substring(subStringStart));
-
-        System.out.println();
     }
-
-    private static void printText(String text) {
-        if (!text.isEmpty())
-            System.out.printf("TEXT(\"%s\")", text);
-    }
-
-    private static void printFormat(String format) {
-        System.out.printf("FORMAT(\"%s\")", format);
-    }
-
 }
