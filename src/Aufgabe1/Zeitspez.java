4b076aa3b442498ec53e3184921b237b21a61c81
Sebastian Hirsig <sebastian.hirsig@htwg-konstanz.de>
2022-10-16 19:03:48 +0200

    1b: Worked on

--- src/Aufgabe1/Zeitspez.java
+++ src/Aufgabe1/Zeitspez.java
@@ -12,16 +12,25 @@ public class Zeitspez {
         }
 
 
-        String formatString = "Wochentag: %tA Uhrzeit: %tT";//args[0];
+        String formatString = "11:00 a. m.";//args[0];
+        /* 12 Hour Clock
+
+            12:00 a.m.
+            12 midnight
+            12 noon
+            12:01 p.m.
+
+            (((0[1-9])|(1[1-2])):([0-5][0-9]) ((a. m.)|(p. m.)))|(12:00 (noon|midnight))
 
 
-        String PATTERN = "(" +  ")|";
+         */
+        String PATTERN = "(((0[1-9])|(1[1-2])):([0-5][0-9]) ((a. m.)|(p. m.)))|(12:00 (noon|midnight))";
 
         Pattern pattern = Pattern.compile(PATTERN);
         Matcher matcher = pattern.matcher(formatString);
 
         while (matcher.find()) {
-            System.out.print(matcher.group());
+            System.out.print("Found: " + matcher.group());
         }
     }
 }
