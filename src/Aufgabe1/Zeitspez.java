25f942e02e5d074ba3373322bfb94ac7376098fa
Sebastian Hirsig <sebastian.hirsig@htwg-konstanz.de>
2022-10-16 18:55:55 +0200

    1b: initialized

--- src/Aufgabe1/Zeitspez.java
+++ src/Aufgabe1/Zeitspez.java
@@ -1,4 +1,27 @@
 package Aufgabe1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Zeitspez {
+
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.err.println("java RegexTest <format-string>");
+            System.exit(1);
+        }
+
+
+        String formatString = "Wochentag: %tA Uhrzeit: %tT";//args[0];
+
+
+        String PATTERN = "(" +  ")|";
+
+        Pattern pattern = Pattern.compile(PATTERN);
+        Matcher matcher = pattern.matcher(formatString);
+
+        while (matcher.find()) {
+            System.out.print(matcher.group());
+        }
+    }
 }
