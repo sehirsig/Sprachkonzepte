75ae255e904a7533de8b33e9821a812b12eb7c26
YannickSchall <yannickschall@gmx.de>
2022-11-16 13:44:52 +0100

    3a)

--- src/Aufgabe2/OURToAst.java
+++ src/Aufgabe2/OURToAst.java
@@ -4,6 +4,8 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import static java.lang.System.exit;
+
 public final class OURToAst {
     private OURToAst() { }
 
@@ -14,10 +16,32 @@ public final class OURToAst {
         if (parser.getNumberOfSyntaxErrors() > 0) {
             System.err.printf("%d error(s) detected%n",
                               parser.getNumberOfSyntaxErrors());
-            System.exit(1);
+            exit(1);
         }
 
         OUR ast = new OURBuilder().build(tree);
+        if (!testStaticSemantic(ast.toString())) exit(1);
+
         System.out.printf("OURtoString() = \"%s\"%n", ast);
     }
+
+    static public boolean testStaticSemantic(String tree) {
+        char[] ourString = tree.toCharArray();
+        char current = ' ';
+        for (char c : ourString) {
+            if ((c == '{' || c == '[') && (current != c)) {
+                current = c;
+            } else if (c == '{' || c == '[') {
+                System.out.printf("Statisch semantischer Fehler: Es dürfen nicht zwei %c aufeinander folgen !\n", current);
+                return false;
+            }
+        }
+        return true;
+        // [22]%{hi}%[22]%{hi}%[22]%{hi}%[22]%{hi}%[22].
+        // [22]%[22]%[22].
+        // [22]%{hi}%[22]%{hi}%[22]%{hi}%[22]%{hi}%{hi}%{hi}.
+        // 0 == gleich -> semantisch falsch
+        // 1 == ungleich -> semantisch richtig
+    }
+
 }
